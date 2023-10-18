2ae292bdde30ea9822e6600297d43c5d5de076bc
Ava Melchior <avacmelchior@gmail.com>
2023-10-18 13:41:41 -0500

    lab 2

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import './App.css';
 
 class HornedBeast extends React.Component {
-  render() {
-    console.log(this.props);
-    return (
-      <>
-       <h2>{this.props.title}</h2>
-       <img src={this.props.imageUrl} alt={this.props.title}/>
-       <p>{this.props.description}</p>
-      </>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+            this.state = {
+                favorites: 0,
+            };
+        }
+
+        handleFavoriteClick = () => {
+            this.setState((prevState) => ({
+                favorites: prevState.favorites + 1,
+            }));
+        };
+
+        render() {
+            console.log(this.props);
+            return (
+                <>
+                    <h2>{this.props.title}</h2>
+                    <img src={this.props.imageUrl} alt={this.props.title} />
+                    <p>{this.props.description}</p>
+                    <button onClick={this.handleFavoriteClick}>
+                        <span role="img" aria-label="heart">❤️</span> Favorites: {this.state.favorites}
+                    </button>
+                </>
+            );
+        }
+    }
 
 export default HornedBeast;
